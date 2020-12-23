c833fead882d8e3e2f8ad14947120842f94f2e22
Hamish Isaacs <hamisaacs@gmail.com>
2020-12-23 21:22:17 +0800

    Added initial index and script files

---
+++ README.md
@@ -0,0 +1,20 @@
+# music-artist-project
+
+iLoveMusic is an app where users can search for their favorite artists and display upcoming events, social media posts, music videos and preview songs. Built using Bandsintown, Spotify, Youtube, facebook and Instagram Api's. 
+
+View the project here => https://alecji.github.io/music-artist-project/
+
+
+
+## Technologies Utilized
+1. [jQuery](https://jquery.com)
+2. [Bootstrap](https://getbootstrap.com/docs/3.3/)
+3. [Masonry](https://masonry.desandro.com/)
+4. [Firebase](https://firebase.google.com/)
+
+## Api's Used
+1. [Bandsintown](http://www.artists.bandsintown.com/bandsintown-api/)
+2. [Spotify](https://developer.spotify.com/web-api/)
+3. [Youtube](https://developers.google.com/youtube/v3/)
+4. [facebook](https://developers.facebook.com/docs/graph-api/)
+5. [Instagram](https://github.com/whizzzkid/instagram-proxy-api)
---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+// $(document).ready(function() {
+
+    // when document ready, hide most of the div and show some div
+    $("#bandsintown, #youtube, #social, #instagram, #recentSearch, #artist-navigation, #spotify, .js-artist-bio, .js-no-events").hide();
+
+    // when the search button executed, run searchBandsInTown function, show most of the div and hide some div
+    $("#searchButton").on("click", function(event) {
+      event.preventDefault();
+
+      $("#bandsintown, #youtube, #social, #instagram, #recentSearch, #artist-navigation, #spotify").show();
+      
+      var artistInput = $("#search").val().trim();
+      $("#feature-carousel").hide();
+      
+      $("#search").val("");
+      
+      searchBandsInTown(artistInput);
+      spotifySearch(artistInput);
+      populateSocial(artistInput);
+
+      window.scrollTo(0, 0);
+    });
+
+    // when carousel clicked, go to that artist
+    $('#carousel').click(function() {
+      var artist = ($(this).attr("artistName"));
+      $("#search").val(artist);
+      $("#searchButton").click();
+    });
+
+    // when read more is clicked
+    $('.js-read-more').click(function(e) {
+      e.preventDefault();
+      $(".js-artist-bio").slideToggle('fast');
+    });
+
+    // recent searches
+    $(document).on("click", ".recentSearchButton, .artist", function() {
+      var artist = ($(this).attr("artistName"));
+      $("#search").val(artist);
+      $("#searchButton").click();
+      window.scrollTo(0, 0);
+    });
+
+    // Calls Carousel
+    $('.carousel').carousel();
+
+    // Calls Tooltip
+    $(document).tooltip({
+      selector: '[data-toggle="tooltip"]'
+    });
+
+    // Smooth Scroll
+    $('.smooth-scroll').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          var navHeight = $("#site-header").height();
+          var pos = target.offset();
+              pos = pos.top - navHeight;
+          $("html,body").animate( { scrollTop: pos },  800 ); return false;
+        }
+      }
+    });
+
+ 
