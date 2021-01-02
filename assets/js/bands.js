11085dba70bf6944de75bf4ffa78f52743ee7901
Hamish Isaacs <hamisaacs@gmail.com>
2021-01-02 15:26:08 +0800

    Update bands.js and include localStorage

--- assets/js/bands.js
+++ assets/js/bands.js
@@ -1,5 +1,3 @@
-
-
 //Variable declaration by grabbing class
 const artistName = document.querySelector(".js-artist-name");
 const artistImage = document.querySelector(".js-artist-image");
@@ -52,22 +50,30 @@ function searchBandsInTown(artistInput) {
         eventNull.style.display = "none";
         const eventInfo = document.querySelector(".event");
         eventInfo.innerHTML = " ";
-        //$(".event").empty();
-        //  eventInfo.empty();
+
         // Loop through results
+        
         $.each(response, function (i, data) {
           if (i <= 10) {
-            let eventDetails = $("<tr>");
-            eventDetails.append("<td class='td'>" + moment(data.datetime).format('MM/DD/YY') + "</td>");
-            eventDetails.append("<td class='td'>" + data.venue.name + "</td>");
-            eventDetails.append("<td class='td'>" + data.venue.city + ", " + data.venue.country + "</td>");
-            eventDetails.append('<td class="btn btn-sm btn-primary btn-block">Save</a></td>');
-            $(".event").append(eventDetails);
+            let event = $("<tr>");
+            let isSaved_ = isSaved(moment(data.datetime).format('MM/DD/YY'), data.venue.name, data.venue.city);
+           
+            let tett = isSaved_ ? "saved" : "save";
+                event.append("<td class='td'>" + moment(data.datetime).format('MM/DD/YY') + "</td>");
+            event.append("<td class='td'>" + data.venue.name + "</td>");
+            event.append("<td class='td'>" + data.venue.city + ", " + data.venue.country + "</td>");
+            event.append('<td class="btn btn-sm btn-primary btn-block" onclick="saveToBookmark(this,'
+                + "'"+ moment(data.datetime).format('MM/DD/YY') +"'"+
+                ","+ "'"+ data.venue.name  +"'"+
+                ","+ "'"+ data.venue.city +"'"+')">' + tett + '</a></td>');
+            $(".event").append(event);
           }
         });
-      }
+
+        }
     }
   });
+
 
   // run WIKIPEDIA API to get more info about the artist
   $.ajax({
@@ -83,29 +89,83 @@ function searchBandsInTown(artistInput) {
     headers: {
       'Api-User-Agent': 'CBC'
     },
-   
+    error: function () {},
     success: function (data) {
       let markup = data.parse.text["*"];
       let i = $('<div>').html(markup);
       i.find('a').each(function () {
         $(this).replaceWith($(this).html());
       });
-      i.find('sup').remove();
-      i.find('.mw-ext-cite-error').remove();
+      // i.find('sup').remove();
+      // i.find('.mw-ext-cite-error').remove();
 
       $('.js-artist-bio').html($(i).find('p'));
 
-      var article = $('.js-artist-bio').html();
+      let article = $('.js-artist-bio').html();
 
-      var unavailable = "<p>Redirect to:</p>";
+      let unavailable = "<p>Redirect to:</p>";
       if (article === unavailable) {
         $('.js-artist-bio').html("Sorry, we are unable to find the article.")
       }
-    },
-    error: function () {}
-
+    }
   });
-
-
-
 }
+
+
+// Save item in localStorage to bookmark
+function  isSaved (time, name, city) {
+
+  if (!localStorage.getItem("bookmarkItems")) {
+    localStorage.setItem("bookmarkItems", "[]");
+  }
+ 
+  let oldStorage = JSON.parse(localStorage.getItem("bookmarkItems"));
+  let found = false;
+
+  for(let i in oldStorage) {
+    if (time == oldStorage[i][0] && name == oldStorage[i][1] && city == oldStorage[i][2])  {
+      found = true;
+      break;
+    }
+  }
+  return found;
+}
+
+// Saving artist to Bookmark
+function  saveToBookmark (el, time, name, city) {
+
+  el.innerText ="saved";
+
+  if (!localStorage.getItem("bookmarkItems")) {
+    localStorage.setItem("bookmarkItems", "[]");
+  }
+
+  if (isSaved(time, name, city)) {
+    el.innerText ="save";
+    remove(time, name, city);
+    return ;
+  }
+
+  let oldStorage = JSON.parse(localStorage.getItem("bookmarkItems"));
+  oldStorage.push([time, name, city]);
+  localStorage.setItem("bookmarkItems", JSON.stringify(oldStorage));
+  console.log(time, name, city);
+}
+
+// Removing item from localStorage Bookmark
+function  remove (time, name, city) {
+
+  if (!localStorage.getItem("bookmarkItems")) {
+    localStorage.setItem("bookmarkItems", "[]");
+  }
+
+  let oldStorage = JSON.parse(localStorage.getItem("bookmarkItems"));
+
+  for(let i in oldStorage) {
+    if (time == oldStorage[i][0] && name == oldStorage[i][1] && city == oldStorage[i][2])  {
+      oldStorage.splice(i, 1);
+      localStorage.setItem("bookmarkItems", JSON.stringify(oldStorage));
+    }
+  }
+}
+
