2bd535a085dc7615362136cbd4dc0590cb6c604e
Hamish Isaacs <hamisaacs@gmail.com>
2021-01-07 15:13:59 +0800

    Hiding the chevrons after loader runs

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,18 +1,24 @@
-
 // This is a loader that will display carousel after 4 seconds
 document.querySelector('.carousel').style.display = "none";
 document.querySelector('.div1').classList.add('progress');
 document.querySelector('.div2').classList.add('indeterminate');
-const hideBgL = document.getElementById("chevron-left");
-hideBgL.style.visibility = "hidden";
-const hideBgR = document.getElementById("chevron-right");
-hideBgR.style.visibility = "hidden";
+const hideChevBgL = document.getElementById("chevron-left");
+hideChevBgL.style.visibility = "hidden";
+const hideChevBgR = document.getElementById("chevron-right");
+hideChevBgR.style.visibility = "hidden";
 
-const hideCarousel = document.getElementById('feature-carousel');
-hideCarousel.style.display = "none";
+const hideFeatureCarousel = document.getElementById('feature-carousel');
+hideFeatureCarousel.style.display = "none";
 
-setTimeout(function() {
+setTimeout(function () {
     document.querySelector('.div1').classList.remove('progress');
     document.querySelector('.div2').classList.remove('indeterminate');
     document.querySelector('.carousel').style.display = "block";
+    const hideChevBgL = document.getElementById("chevron-left");
+    hideChevBgL.style.visibility = "visible";
+    const hideChevBgR = document.getElementById("chevron-right");
+    hideChevBgR.style.visibility = "visible";
+
+    const hideFeatureCarousel = document.getElementById('feature-carousel');
+    hideFeatureCarousel.style.display = "block";
 }, 2000);
