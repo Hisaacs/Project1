c4f59c396a082604429c8f997dc98e2231e59597
Hamish Isaacs <hamisaacs@gmail.com>
2021-01-07 14:54:10 +0800

    Update bands.js and Loader.js Also add default image

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -3,6 +3,13 @@
 document.querySelector('.carousel').style.display = "none";
 document.querySelector('.div1').classList.add('progress');
 document.querySelector('.div2').classList.add('indeterminate');
+const hideBgL = document.getElementById("chevron-left");
+hideBgL.style.visibility = "hidden";
+const hideBgR = document.getElementById("chevron-right");
+hideBgR.style.visibility = "hidden";
+
+const hideCarousel = document.getElementById('feature-carousel');
+hideCarousel.style.display = "none";
 
 setTimeout(function() {
     document.querySelector('.div1').classList.remove('progress');
